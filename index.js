547c975665ce4ef5821897b75ae023d62844ceee
kon <74819209+kon2300@users.noreply.github.com>
2020-12-05 11:29:50 +0900

    関数名を修正。文字列型を数値型に修正。

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ function addBtn() {
   const fizz = document.getElementById("fizz").value;
   const buzz = document.getElementById("buzz").value;
   const answer = document.getElementById("answer");
-  
+  fizz - 0;
+  buzz - 0;
 
 
-  const erase = () => {  
+  const eraseFizzBuzz = () => {  
     if (answer.hasChildNodes()) {
       for(let i = answer.childNodes.length -1; i >= 0; i --) {
         answer.removeChild(answer.childNodes[i]);
@@ -15,7 +16,7 @@ function addBtn() {
     }
   };
 
-  const culculation = () => {  
+  const culculationFizzBuzz = () => {  
     for (let i = 1; i < 100; i++) {
       const p = document.createElement("p");
       if (i % fizz === 0 && i % buzz === 0) {
@@ -29,24 +30,22 @@ function addBtn() {
     }
   };
   
-  const check = (str) => {
+  const integerCheck = (str) => {
     const reg = new RegExp(/^[0-9]+$/);
     const res = reg.test(str);
     return res;
   };
   
-  if (!check(fizz) && !check(buzz)) {
+  if (!integerCheck(fizz) && !integerCheck(buzz)) {
     answer.textContent = ("");
     answer.textContent = ("整数値を入力してください");
-  } else if (!check(fizz) || !check(buzz)) {
+  } else if (!integerCheck(fizz) || !integerCheck(buzz)) {
     answer.textContent = ("");
     answer.textContent = ("整数値を入力してください");
-  } else if (check(fizz) && check(buzz)) {
+  } else if (integerCheck(fizz) && integerCheck(buzz)) {
     answer.textContent = ("");
-    erase();
-    culculation();
+    eraseFizzBuzz();
+    culculationFizzBuzz();
   };
-
-  
 };
 
